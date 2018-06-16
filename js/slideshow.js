55bb5d4564c14ffe91015ee743be9a1b93c98d28
ASAZING <malcolmedina206@gmail.com>
2018-06-16 17:14:53 -0500

    Update #3

--- js/slideshow.js
+++ js/slideshow.js
@@ -1,14 +1,30 @@
-var myIndex = 0;
-carousel();
+var myIndex = 1;
+carousel(myIndex);
 
-function carousel() {
+// Next/previous controls
+function plusSlides(n) {
+  carousel(myIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  carousel(myIndex = n);
+}
+
+function carousel(n) {
     var i;
     var x = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    if (n > x.length) {myIndex = 1} 
+    if (n < 1) {myIndex = x.length}
     for (i = 0; i < x.length; i++) {
-       x[i].style.display = "none";  
+      x[i].style.display = "none"; 
     }
-    myIndex++;
-    if (myIndex > x.length) {myIndex = 1}    
-    x[myIndex-1].style.display = "block";  
-    setTimeout(carousel, 3000);    
+    for (i = 0; i < dots.length; i++) {
+      /* x[i].style.display = "none";  */
+      dots[i].className = dots[i].className.replace(" active", "");
+    }
+
+    x[myIndex-1].style.display = "block"; 
+    dots[myIndex-1].className += " active";
 }
