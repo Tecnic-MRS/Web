2d8277caca719abaea258a4fc659695847ad0104
ASAZING <malcolmedina206@gmail.com>
2018-06-28 00:28:17 -0500

    Support Online :v

---
+++ js/supportonline.js
@@ -0,0 +1,24 @@
+//wpac comentarios
+wpac_init = window.wpac_init || [];
+wpac_init.push({widget: 'Comment', id: 12331});
+(function() {
+    if ('WIDGETPACK_LOADED' in window) return;
+    WIDGETPACK_LOADED = true;
+    var mc = document.createElement('script');
+    mc.type = 'text/javascript';
+    mc.async = true;
+    mc.src = 'https://embed.widgetpack.com/widget.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(mc, s.nextSibling);
+})();
+//Chatra
+(function(d, w, c) {
+    w.ChatraID = 'RXw7D6yDwoPfiWnLH';
+    var s = d.createElement('script');
+    w[c] = w[c] || function() {
+        (w[c].q = w[c].q || []).push(arguments);
+    };
+    s.async = true;
+    s.src = 'https://call.chatra.io/chatra.js';
+    if (d.head) d.head.appendChild(s);
+})(document, window, 'Chatra');
+
